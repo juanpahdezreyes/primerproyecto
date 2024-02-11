5e88217e0ac95bec2da96473b81d5b6b5097f5e0
juanpahdezreyes <Juanpahdez.r@MacBook-Air-de-Tecnologia.local>
2024-02-11 07:39:58 -0600

    mi protecto cpp

---
+++ proyecto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int nip;
+int main ()
+{
+    cout << "bienvenido a santander" << endl;
+    cout << "ingrese su nip"<< endl;
+    cin >> nip;
+    cout << "su nip es correcto" << endl;
+    cout << "que desea realizar" << endl;
+    cout << "su nip es 2456"<< endl;
+
+}
+return 0;
